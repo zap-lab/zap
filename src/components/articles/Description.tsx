286e11ed3e3b334046fba34c7eff9f5b956af159
devgw <doscmdev@gmail.com>
2020-06-20 16:13:09 +0900

    feat: add basic article components

---
+++ src/components/articles/Description.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    description: string;
+}
+
+const Description: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Description;
---
+++ src/components/articles/Title.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    title: string;
+}
+
+const Title: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Title;
